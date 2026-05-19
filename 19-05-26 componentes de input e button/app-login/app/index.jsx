162acdc3a114e4b4287d21d06922525efb2671de
Seu Nome <arthurenricohidalgo005@gmail.com>
2026-05-19 10:30:30 -0300

    Primeiro commit

---
+++ 19-05-26 componentes de input e button/app-login/app/index.jsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import Login from './pages/login';
+import Nike from './pages/nike';
+
+export default function Index() {
+  return (
+    <Login />
+    //<Nike />
+  )
+}
