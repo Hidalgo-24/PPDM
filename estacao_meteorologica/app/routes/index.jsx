9bba187fad1cf5610020e2a45e1fb7b068fb8cbc
Seu Nome <arthurenricohidalgo005@gmail.com>
2026-06-09 07:35:52 -0300

    Atualização do projeto

--- estacao_meteorologica/app/routes/index.jsx
+++ estacao_meteorologica/app/routes/index.jsx
@@ -4,7 +4,7 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 //import das telas
 import Login from '../pages/login';
-import Cadastro from '../pages/cadastro';
+import Registro from '../pages/registro';
 
 
 //import dos navegadores
@@ -26,7 +26,7 @@ function MenuSuperior() {
                         nomeIcone = focused ? 'audit' : 'audit-outline';
                     }
 
-                    if (route.name === 'Cadastro') {
+                    if (route.name === 'Registro') {
                         nomeIcone = focused ? 'file-add' : 'file-add-outline'
                     }
                     if (route.name === 'Relatório') {
@@ -35,10 +35,10 @@ function MenuSuperior() {
                     return <Ionicons name={nomeIcone} size={size} color={color} />
                 }
             })}
-        >
-            {/* <Draw.Screen name='Sesi - Pet' component={AbasInferiores} options={{headerTitleAlign: 'center'}} />
-            <Draw.Screen name='Atendimentos' component={Atendimentos} />
-            <Draw.Screen name='Profissionais' component={Profissionais} /> */}
+        > 
+            <Draw.Screen name='Home' component={Login} options={{headerTitleAlign: 'center'}} />
+            {/* <Draw.Screen name='Atendimentos' component={Atendimentos} /> */}
+            {/* <Draw.Screen name='Profissionais' component={Profissionais} /> */}
         </Draw.Navigator>
     )
 }
@@ -47,8 +47,8 @@ export default function Rotas() {
     return (
         <Stack.Navigator>
             <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-            <Stack.Screen name='Cadastro' component={Cadastro} options={{ title: 'Cadsatro de usuário' }} />
-            <Stack.Screen name='Principal' component={MenuSuperior} options={{ headerShown: false , title: ''}} />
+            <Stack.Screen name='Registro' component={Registro} options={{ title: 'Cadastro de usuário' }} />
+            {/* <Stack.Screen name='Principal' component={MenuSuperior} options={{ headerShown: false , title: ''}} /> */}
         </Stack.Navigator>
     )
 }
